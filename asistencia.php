97cbc4e18b8b850397830bc905f173f07c9212ef
Davinchi211 <guillermo.mar18@gmail.com>
2023-09-11 23:39:43 -0600

    UPDATE

---
+++ asistencia.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASISTENCIA</title>
+    <link rel="stylesheet" href="style_asistencia.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="shortcut icon" href="img/icon_assis.png">
+</head>
+<body>
+    <nav class="navbar bg-success text-white fixed-top">
+        <div class="container-fluid justify-content-start text-center">
+            <div class="row">
+                <div class="col-2">
+                <a href="style_asistencia.css" class="navbar-brand">
+                    <svg width="40" height="40">
+                     <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+                    <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+             </svg>  
+          </svg>
+        </a>
+                </div>
+                <div class="col-10">
+                  <h3>DIGITAL CHECK</h3>
+                </div>
+            </div>
+            <h6>@usuario</h6>
+        </div>
+    </nav>
+    <!--BODY -->
+    <div class="container-fluid bg-secondary-subtle" id="body">
+        <div class="container text-center">
+            <div class="row align-items-center">
+                <div class="col-2">
+                <ul class="nav nav-pills">
+  <li class="nav-item dropdown bg-dark rounded">
+    <a class="text-warning nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">CURSO1</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="#">CURSO2</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="#">CURSO3</a></li>
+    </ul>
+  </li>
+</ul>
+                </div>
+                <div class="col-10">
+                    <h1>LISTA ALUMNOS</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--FOOTER-->
+    <div class="container-fluid bg-info-subtle">
+        footer
+    </div>
+</body>
+</html>
