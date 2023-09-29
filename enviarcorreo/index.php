e57acccd435f09e4b279a00ff378ba7bf182ec92
Davinchi211 <guillermo.mar18@gmail.com>
2023-09-28 23:26:45 -0600

    .

--- enviarcorreo/index.php
+++ enviarcorreo/index.php
@@ -4,6 +4,7 @@
     <title>Correo</title>
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <link rel="icon" href="../public/img/mail.svg">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <body>
 <h1> Enviar nuevo correo </h1>
